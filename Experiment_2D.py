93aeb4f3a4a68fb6d91cec83bdb550f4afdbd9b4
EduPH <epaluzohidalgo@gmail.com>
2023-11-01 18:59:45 +0100

    plots

--- Experiment_2D.py
+++ Experiment_2D.py
@@ -8,22 +8,30 @@ X,y=make_classification(200, n_features=2, n_redundant=0,n_classes=2,class_sep=1
 
 
 sm1 = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=0)
-m1 = sm1.get_n_splits(X, y)
+m1 = sm1.get_n_splits(X, y) #TBD
 
 m2 = maxmin_selector(X,y,0.2,p=np.inf)
 
 m3 = getPHLandmarks(X, 0.4, 0.2, scoring_version='restrictedDim', dimension=1, landmark_type="representative")
 
-m5 = dominatingSet(X,y,epsilon=0.2)
+m5 = dominatingSet(X,y,epsilon=0.4)
 
 
 subset2 = X[m2]
 subset3 = m3[0]
 subset5 = X[m5]
 
-plt.plot(subset2[:,0],subset2[:,1],'.')
+
+
+plt.scatter(X[:,0],X[:,1],c=y)
+plt.scatter(subset2[:,0],subset2[:,1],marker='x',s=20,c='r')
+plt.title("Maxmin")
 plt.show()
-plt.plot(subset3[:,0],subset3[:,1],'.')
+plt.scatter(X[:,0],X[:,1],c=y)
+plt.scatter(subset3[:,0],subset3[:,1],marker='x',s=20,c='r')
+plt.title("PH landmarks")
 plt.show()
-plt.plot(subset5[:,0],subset5[:,1],'.')
+plt.scatter(X[:,0],X[:,1],c=y)
+plt.scatter(subset5[:,0],subset5[:,1],marker='x',s=20,c='r')
+plt.title("Representative dataset")
 plt.show()
