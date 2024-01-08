c004dd7043609d8ff331ba4883ccba1f256b25c5
Javier Perera-Lago <92569314+JPereraLago@users.noreply.github.com>
2024-01-08 21:34:16 +0100

    Create clone_repos.bat

---
+++ Original_repositories/clone_repos.bat
@@ -0,0 +1,31 @@
+::Use this folder to store the codes used in this survey.
+
+::DOM selection
+git clone https://github.com/Cimagroup/Experiments-Representative-datasets
+
+::PHL selection
+git clone https://github.com/stolzbernadette/Outlier-robust-subsampling-techniques-for-persistent-homology
+
+::NRMD selection
+git clone https://github.com/bghojogh/Prototype-Selection-Matrix-Decomposition
+
+::PSA selection
+git clone https://github.com/bghojogh/Principal-Sample-Analysis
+
+::PRD selection
+git clone https://github.com/Trusted-AI/AIX360.git
+
+::SVP
+git clone https://github.com/stanford-futuredata/selection-via-proxy
+
+::FES selection
+git clone https://github.com/mtoneva/example_forgetting
+
+::GRAND, EL2N selection
+git clone https://github.com/mansheej/data_diet
+
+::IF selection
+git clone https://github.com/nimarb/pytorch_influence_functions
+
+::Dataset Distillation
+git clone https://github.com/ssnl/dataset-distillation
