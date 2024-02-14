b78ac8ef69cdc7b039fe8809a766b68a7ad259f5
Javier Perera-Lago <92569314+JPereraLago@users.noreply.github.com>
2024-02-14 09:30:36 +0100

    Add files via upload

---
+++ data_reduction/Original_repositories/clone_repos.sh
@@ -0,0 +1,13 @@
+::Use this folder to store the codes used in this survey.
+
+::PHL selection
+git clone https://github.com/stolzbernadette/Outlier-robust-subsampling-techniques-for-persistent-homology
+
+::NRMD selection
+git clone https://github.com/bghojogh/Prototype-Selection-Matrix-Decomposition
+
+::PSA selection
+git clone https://github.com/bghojogh/Principal-Sample-Analysis
+
+::PRD selection
+git clone https://github.com/Trusted-AI/AIX360.git
