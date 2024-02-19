05cead420e3124e7abe36f8471cd5b29825abac0
Javier Perera-Lago <92569314+JPereraLago@users.noreply.github.com>
2024-02-19 09:18:51 +0100

    Update wrapper.py

--- data_reduction/wrapper.py
+++ data_reduction/wrapper.py
@@ -5,34 +5,29 @@ import torch
 from torch.utils.data import DataLoader, TensorDataset
 ###################################
 
-# def fes_selection(X,y,current_accuracy, forgetting_events,perc):
-#     n_y = len(y)
-#     for i in range(n_y):
-#         if current_accuracy[i]==0 and forgetting_events[i]==0:
-#             forgetting_events[i]=np.inf
-#     n = math.trunc(n_y*perc)
-#     indices = np.argsort(forgetting_events)[-n:]
-#     X_res = X[indices]
-#     y_res = y[indices]
-#     return X_res, y_res
-
-def fes_selection(X,y,current_accuracy,forgetting_events,perc):
+def fes_selection(y, current_accuracy, forgetting_events, perc, maximum, X=None):
     classes = np.unique(y)
-    indices = np.array([],dtype='int32')
+    indices = np.array([], dtype='int32')
     n_y = len(y)
+    
     for cl in classes:
-        X_cl = X[y==cl]
-        y_cl = y[y==cl]
+        y_cl = y[y == cl]
         n = len(y_cl)
-        indices_cl = np.arange(n_y)[y==cl]
-        current_cl = current_accuracy[y==cl]
-        forgetting_cl = forgetting_events[y==cl]
+        indices_cl = np.arange(n_y)[y == cl]
+        current_cl = current_accuracy[y == cl]
+        forgetting_cl = forgetting_events[y == cl]
+
         for i in range(n):
-            if current_cl[i]==0 and forgetting_cl[i]==0:
-                forgetting_cl[i]=np.inf
-        n_cl = math.trunc(n*perc)
+            if current_cl[i] == 0 and forgetting_cl[i] == 0:
+                forgetting_cl[i] = maximum
+        
+        n_cl = math.trunc(n * perc)
         indices_selected = indices_cl[np.argsort(forgetting_cl)[-n_cl:]]
-        indices = np.concatenate((indices,indices_selected))
-    X_res = X[indices]
-    y_res = y[indices]
-    return X_res, y_res
+        indices = np.concatenate((indices, indices_selected))
+    
+    if X is not None:
+        X_res = X[indices]
+        y_res = y[indices]
+        return X_res, y_res
+    else:
+        return indices
