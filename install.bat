9d393d0f710b090eaf17fca1da7eb0170022d232
Javier Perera-Lago <92569314+JPereraLago@users.noreply.github.com>
2024-02-27 17:13:30 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -3,11 +3,6 @@
 call rmdir /s /q "data_reduction/Original_repositories/AIX360/aix360/models/DIPVAE"
 
 echo #################################
-echo ripser and faiss-cpu
-echo #################################
-call conda install -c conda-forge -c pytorch/label/nightly ripser==0.6.4 faiss-cpu
-
-echo #################################
 echo torch, torchvision and torchaudio with cuda
 echo #################################
 call pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu113
