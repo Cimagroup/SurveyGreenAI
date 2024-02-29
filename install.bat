f0b3d90506dc3f709189a73fa86db8905afa0782
Javier Perera-Lago <92569314+JPereraLago@users.noreply.github.com>
2024-02-29 16:30:57 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -5,7 +5,7 @@ call rmdir /s /q "data_reduction/Original_repositories/AIX360/aix360/models/DIPV
 echo #################################
 echo torch, torchvision and torchaudio with cuda
 echo #################################
-call pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu113
+call pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 
 echo #################################
 echo install data_reduction
