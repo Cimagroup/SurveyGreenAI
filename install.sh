509d0d146cdf35f456308249458bbe914330de93
Javier Perera-Lago <92569314+JPereraLago@users.noreply.github.com>
2024-02-29 16:26:01 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ rm -rf "data_reduction/Original_repositories/AIX360/aix360/models/DIPVAE"
 echo "#################################"
 echo "torch, torchvision, and torchaudio with cuda"
 echo "#################################"
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu113
 
 echo "#################################"
 echo "install data_reduction"
